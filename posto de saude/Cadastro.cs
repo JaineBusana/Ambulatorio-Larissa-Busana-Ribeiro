8fe914b3c5454b33f93289ff5c7c3189ca4ff134
Larissa Beatriz Costa <larissa.beatrizzcosta@gmail.com>
2023-10-03 21:01:06 -0300

    Comentei a classe Cadastro

--- posto de saude/Cadastro.cs
+++ posto de saude/Cadastro.cs
@@ -1,97 +1,97 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿//using System;
+//using System.Collections.Generic;
+//using System.Linq;
+//using System.Text;
+//using System.Threading.Tasks;
 
-namespace posto_de_saude
-{
-    internal class Cadastro
-    {
-        public void CadastroPessoa()
-        {
-            Pessoa novaPessoa = new Pessoa();
-            Console.WriteLine("Informe o nome do funcionário:");
-            novaPessoa.Nome = Console.ReadLine();
-            Console.WriteLine("Idade:");
-            novaPessoa.Idade = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Rua:");
-            novaPessoa.Rua = Console.ReadLine();
-        }
-        public void CadastroFuncionario()
-        {
-            Console.WriteLine("Informe o nome do funcionário:");
-            string nomeFuncionario = Console.ReadLine();
-            Console.WriteLine("Idade:");
-            int idadeFuncionario = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Rua:");
-            string ruaFuncionario = Console.ReadLine();
-            Console.WriteLine("Cargo:");
-            string cargoFuncionario = Console.ReadLine();
-            Console.WriteLine("Turno:");
-            string turnoFuncionario = Console.ReadLine();
-            Console.WriteLine("Informe a matricula");
-            int matriculaFuncionario = Convert.ToInt32(Console.ReadLine());
+//namespace posto_de_saude
+//{
+//    internal class Cadastro
+//    {
+//        public void CadastroPessoa()
+//        {
+//            Pessoa novaPessoa = new Pessoa();
+//            Console.WriteLine("Informe o nome do funcionário:");
+//            novaPessoa.Nome = Console.ReadLine();
+//            Console.WriteLine("Idade:");
+//            novaPessoa.Idade = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Rua:");
+//            novaPessoa.Rua = Console.ReadLine();
+//        }
+//        public void CadastroFuncionario()
+//        {
+//            Console.WriteLine("Informe o nome do funcionário:");
+//            string nomeFuncionario = Console.ReadLine();
+//            Console.WriteLine("Idade:");
+//            int idadeFuncionario = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Rua:");
+//            string ruaFuncionario = Console.ReadLine();
+//            Console.WriteLine("Cargo:");
+//            string cargoFuncionario = Console.ReadLine();
+//            Console.WriteLine("Turno:");
+//            string turnoFuncionario = Console.ReadLine();
+//            Console.WriteLine("Informe a matricula");
+//            int matriculaFuncionario = Convert.ToInt32(Console.ReadLine());
 
-            Funcionario novoFuncionario = new Funcionario(nomeFuncionario, idadeFuncionario, ruaFuncionario, 
-                cargoFuncionario, turnoFuncionario, matriculaFuncionario);
-        }
+//            Funcionario novoFuncionario = new Funcionario(nomeFuncionario, idadeFuncionario, ruaFuncionario, 
+//                cargoFuncionario, turnoFuncionario, matriculaFuncionario);
+//        }
 
-        public void CadastroMaterial()
-        {
-            Material novoMaterial = new Material();
-            Console.WriteLine("Informe o nome do material:");
-            novoMaterial.Nome = Console.ReadLine();
-            Console.WriteLine("Quantidade no estoque:");
-            novoMaterial.QtdEstoque = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Informe sua indicação:");
-            novoMaterial.Indicacao = Console.ReadLine();
-            Console.WriteLine("Informe seu mês de validade:");
-            novoMaterial.MesValidade = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Informe seu ano de validade:");
-            novoMaterial.AnoValidade = Convert.ToInt32(Console.ReadLine());
-        }
+//        public void CadastroMaterial()
+//        {
+//            Material novoMaterial = new Material();
+//            Console.WriteLine("Informe o nome do material:");
+//            novoMaterial.Nome = Console.ReadLine();
+//            Console.WriteLine("Quantidade no estoque:");
+//            novoMaterial.QtdEstoque = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Informe sua indicação:");
+//            novoMaterial.Indicacao = Console.ReadLine();
+//            Console.WriteLine("Informe seu mês de validade:");
+//            novoMaterial.MesValidade = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Informe seu ano de validade:");
+//            novoMaterial.AnoValidade = Convert.ToInt32(Console.ReadLine());
+//        }
 
-        public void CadastroMedicamento()
-        {
-            Console.WriteLine("Informe o nome do medicamento:");
-            string nomeMedicamento = Console.ReadLine();
-            Console.WriteLine("Quantidade no estoque:");
-            int qtdEstoqueMedicamento = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Informe sua indicação:");
-            string indicacaoMedicamento = Console.ReadLine();
-            Console.WriteLine("Informe seu mês de validade:");
-            int mesValidadeMedicamento = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Informe seu ano de validade:");
-            int anoValidadeMedicamento = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Informe a dosagem do medicamento:");
-            string dosagemMedicamento = Console.ReadLine();
-            Console.WriteLine("Informe a via de administração:");
-            string viaDeAdministracaoMedicamento = Console.ReadLine();
+//        public void CadastroMedicamento()
+//        {
+//            Console.WriteLine("Informe o nome do medicamento:");
+//            string nomeMedicamento = Console.ReadLine();
+//            Console.WriteLine("Quantidade no estoque:");
+//            int qtdEstoqueMedicamento = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Informe sua indicação:");
+//            string indicacaoMedicamento = Console.ReadLine();
+//            Console.WriteLine("Informe seu mês de validade:");
+//            int mesValidadeMedicamento = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Informe seu ano de validade:");
+//            int anoValidadeMedicamento = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Informe a dosagem do medicamento:");
+//            string dosagemMedicamento = Console.ReadLine();
+//            Console.WriteLine("Informe a via de administração:");
+//            string viaDeAdministracaoMedicamento = Console.ReadLine();
 
-            Medicamento novoMedicamento = new Medicamento(nomeMedicamento, qtdEstoqueMedicamento, indicacaoMedicamento, 
-                mesValidadeMedicamento, anoValidadeMedicamento, dosagemMedicamento, viaDeAdministracaoMedicamento);
-        }
+//            Medicamento novoMedicamento = new Medicamento(nomeMedicamento, qtdEstoqueMedicamento, indicacaoMedicamento, 
+//                mesValidadeMedicamento, anoValidadeMedicamento, dosagemMedicamento, viaDeAdministracaoMedicamento);
+//        }
 
-        public void CadastroTriagem()
-        {
-            Console.WriteLine("Informe o nome do paciente:");
-            // vai ter que buscar o arquivo de pacientes
-            string paciente = Console.ReadLine();
-            Console.WriteLine("Informe o peso do paciente:");
-            int peso = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Pressão arterial:");
-            int pressao = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Altura:");
-            int altura = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Temperatura:");
-            int temperatura = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Saturação:");
-            int saturacao = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Quais os sintomas do paciente:");
-            string sintomas = Console.ReadLine();
-        }
+//        public void CadastroTriagem()
+//        {
+//            Console.WriteLine("Informe o nome do paciente:");
+//            // vai ter que buscar o arquivo de pacientes
+//            string paciente = Console.ReadLine();
+//            Console.WriteLine("Informe o peso do paciente:");
+//            int peso = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Pressão arterial:");
+//            int pressao = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Altura:");
+//            int altura = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Temperatura:");
+//            int temperatura = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Saturação:");
+//            int saturacao = Convert.ToInt32(Console.ReadLine());
+//            Console.WriteLine("Quais os sintomas do paciente:");
+//            string sintomas = Console.ReadLine();
+//        }
 
 
-    }
-}
+//    }
+//}
