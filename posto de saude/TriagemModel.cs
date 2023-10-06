581c66bc7f9a561b82c39d340c86d1519b0b9230
Thiago Ribeiro <thiago.devjr@gmail.com>
2023-10-06 19:19:30 -0300

    Criado modelo crud em TriagemModel

--- posto de saude/TriagemModel.cs
+++ posto de saude/TriagemModel.cs
@@ -8,6 +8,38 @@ namespace posto_de_saude
 {
     internal class TriagemModel
     {
+        private List<Triagem> triagem = new List<Triagem>();
+
+
+        public void Criar()
+        {
+            Triagem cadastro = new Triagem();
+            cadastro.Popular();
+            triagem.Add(cadastro);   
+        }
+        public void Ler()
+        {
+            for (int i = 0; i < triagem.Count; i++)
+            {
+                Console.WriteLine(i);
+                triagem[i].Listar();
+            }
+        }
+        public void Alterar()
+        {
+            Ler();
+            Console.WriteLine("Digite o codigo do item a ser alterado");
+            int index = Convert.ToInt32(Console.ReadLine());
+            triagem[index].Popular();
+        }
+
+        public void Deletar()
+        {
+            Ler();
+            Console.WriteLine("Digite o codigo do item a ser deletado");
+            int index = Convert.ToInt32(Console.ReadLine());
+            triagem.RemoveAt(index);
+        }
 
     }
 }
